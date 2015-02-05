5832d5cca1584ce89d39f9849463ffce1a0683e7
Bill Erickson <berickxx@gmail.com>
2015-02-05 14:41:31 -0500

    additional license headers
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- src/pysip2/client.py
+++ src/pysip2/client.py
@@ -12,7 +12,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # -----------------------------------------------------------------------
-
 import sys, socket, random, time, logging
 from pysip2.spec import MessageSpec as mspec
 from pysip2.spec import FieldSpec as fspec
